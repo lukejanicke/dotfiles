855f73a7eb1afbf75728f1c19b07b7a6ff694f67
Luke Janicke <lukejanicke@outlook.com>
2024-12-20 13:02:23 +0800

    tidy wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,10 +4,6 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- Fish
--- See Fish and Zsh configuration for dynamic setting of default system shell
---config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
-
 -- Catppuccin Theme
 function scheme_for_appearance(appearance)
 	if appearance:find('Dark') then
@@ -19,7 +15,7 @@ end
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 -- Fonts
-config.font = wezterm.font('FiraCode Nerd Font Mono')
+config.font = wezterm.font('IosevkaCustomSansTerminal Nerd Font Mono')
 config.font_size = 13
 
 -- Window
