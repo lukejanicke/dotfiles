104bfbe2eacb325439e4fc234410d51bd7cd6ee2
Luke Janicke <lukejanicke@outlook.com>
2025-01-21 14:01:20 +0800

    new zsh way

---
+++ .config/zsh/zprofile/homebrew.zsh
@@ -0,0 +1,3 @@
+# ~/.config/zsh/zprofile/homebrew.zsh
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
---
+++ .config/zsh/zshrc/starship.zsh
@@ -0,0 +1,4 @@
+# ~/.config/zsh/zshrc/starship.zsha
+
+export STARSHIP_CONFIG="$HOME/.config/starship/config.toml"
+eval "$(starship init zsh)"
